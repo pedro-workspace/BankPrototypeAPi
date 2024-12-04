8dfde9b2e9db474d076481e4f5d4d28e6f66b357
Pedro Mariano de Souza <pedroworkspace@gmail.com>
2024-12-04 10:16:35 -0300

    iniciando projeto

---
+++ src/main/kotlin/com/bankapi/bankapiprototype/BankapiprototypeApplication.kt
@@ -0,0 +1,11 @@
+package com.bankapi.bankapiprototype
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BankapiprototypeApplication
+
+fun main(args: Array<String>) {
+	runApplication<BankapiprototypeApplication>(*args)
+}
---
+++ src/test/kotlin/com/bankapi/bankapiprototype/BankapiprototypeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bankapi.bankapiprototype
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BankapiprototypeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
